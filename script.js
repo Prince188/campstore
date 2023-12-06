72f384e30520028b4021a1741bb915c5eeb57410
Prince188 <120271895+Prince188@users.noreply.github.com>
2023-12-06 18:04:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var a= document.getElementById('vm');
+
+function viewmore(){
+    if(a.style.height!=='100%'){
+        a.style.height="100%";
+    }
+    else{
+        a.style.height="85px";
+    }
+}
